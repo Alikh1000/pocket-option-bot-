f3fe6425d1cb3014aed91f23e96338fde2037781
Alikh1000 <153544387+Alikh1000@users.noreply.github.com>
2025-07-31 22:00:50 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+API_KEYS = {
+    "alpha_vantage": [
+        "2EA6SDMJMTDIBTZ6",
+        "d25qd1hr01qhge4dsqngd25qd1hr01qhge4dsqo0"
+    ],
+    "current_key_index": 0
+}
+
+TELEGRAM_TOKEN = "8498625183:AAEA1qRLCFn5f1eCgq1_8MXd1b7b6vuYbaM"
+CHAT_ID = "7695345464"
+
+TIMEFRAMES = ["1min", "15s", "30s"]
+SYMBOLS = {
+    "OTC": ["EUR/USD", "GBP/USD", "USD/JPY"],
+    "MAIN": ["BTC/USD", "ETH/USD", "GOLD"]
+}
+
+# مسیر ذخیره‌سازی کش (به صورت نسبی)
+OFFLINE_CACHE_FILE = "pocket_cache.pkl"
+CACHE_DURATION = 86400  # 24 ساعت
